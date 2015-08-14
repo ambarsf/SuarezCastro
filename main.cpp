cd9c4354600ee64216062db56c8fa84214b7cd6c
Ambar Suarez <ambarsuarez@unitec.edu>
2015-08-13 21:50:15 -0600

    Add Reading Files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <cctype>
+#include <cstring>
 
 using namespace std;
 char Gender();
@@ -33,7 +36,70 @@ struct Calls{
 };
 
 int main(int argc, char* argv[]){
-	return 0;
+
+	// Read Clients
+	char** ClientName=new char* [500];
+	char client[40];
+	ifstream namefile ("RandomNames.txt");
+	if (namefile.is_open()){
+		for (int i = 0; i < 500; i++)
+		{
+			namefile.getline(client,sizeof(client));
+			ClientName[i]=new char[strlen(client)+1];
+			strcpy(ClientName[i],client);
+		}
+		namefile.close();
+	}else{
+		cout<<"Unable to open file"<<endl;
+	}
+
+	// Read Cities
+	char** CityName=new char*[30];
+	char city[40];
+	ifstream cityfile("RandomCities.txt");
+	if (cityfile.is_open()){
+		for (int i = 0; i < 30; i++)
+		{
+			cityfile.getline(city,sizeof(city));
+			CityName[i]=new char[strlen(city)+1];
+			strcpy(CityName[i],city);
+		}
+		cityfile.close();
+	}else{
+		cout<<"Unable to open file"<<endl;
+	}
+
+	// Read Phone Numbers
+	char** PhoneNumbers=new char*[750];
+	char numbers[8];
+	ifstream phonefile("RandomPhoneNumbers.txt");
+	if (phonefile.is_open()){
+		for (int i = 0; i < 750; i++)
+		{
+			phonefile.getline(numbers,sizeof(numbers));
+			PhoneNumbers[i]=new char[strlen(numbers)+1];
+			strcpy(PhoneNumbers[i],numbers);
+		}
+		phonefile.close();
+	}else{
+		cout<<"Unable to open file"<<endl;
+	}
+
+	// Read Cities ID
+	char** CitiesID=new char*[30];
+	char cityID[3];
+	ifstream CityIDfile("CitiesID.txt");
+	if (CityIDfile.is_open()){
+		for (int i = 0; i < 30; i++)
+		{
+			CityIDfile.getline(cityID,sizeof(cityID));
+			CitiesID[i]=new char[strlen(cityID)+1];
+			strcpy(CitiesID[i],cityID);
+		}
+		CityIDfile.close();
+	}else{
+		cout<<"Unable to open file"<<endl;
+	}
 }
 
 char Gender(){
