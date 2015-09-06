9b64f271ec8b2fb55d0be5bba02e3991ada3795e
Ambar Suarez <ambarsuarez@unitec.edu>
2015-09-06 16:17:27 -0600

    end structure of second main

--- options.cpp
+++ options.cpp
@@ -7,17 +7,61 @@ int main (int argc, char* argv[]){
 	int opcion=opciones();
 	switch(opcion){
 		case 1:{
+			int option=menu();
+			switch (option){
+				case 1:{
 
-		};
+				};// end case create
+				case 2:{
+
+				};// end case read
+				case 3:{
+
+				};// end case update
+				case 4:{
+
+				};//end case delete 
+			};
+		};//fin case clients
+
 		case 2:{
+			int option=menu();
+			switch (option){
+				case 1:{
 
-		};
+				};// end case create
+				case 2:{
+
+				};// end case read
+				case 3:{
+
+				};// end case update
+				case 4:{
+
+				};//end case delete 
+			};		
+		};//fin case lines
+
 		case 3:{
+			int option=menu();
+			switch (option){
+				case 1:{
 
-		};
+				};// end case create
+				case 2:{
+
+				};// end case read
+				case 3:{
+
+				};// end case update
+				case 4:{
+
+				};//end case delete 
+			};
+		};// fin case cities
 	};
 	return 0;
-}
+}// end main
 
 int opciones (){
 	int options;
